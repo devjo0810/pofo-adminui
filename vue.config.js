dad455d57d1d90222f5c60d3c13548732c5dc93c
devjo0810 <devjo0810@gmail.com>
2022-12-15 15:19:10 +0900

    파일누락 추가

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: "./docs",
-  publicPath: "/vue-widget-boilerplate",
+  outputDir: "./dist",
+  publicPath: "/",
   css: {
     loaderOptions: {
       scss: {
